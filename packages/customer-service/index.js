62e36ab4608f5848e3d7413cbb16bff6ec7efa13
shishirkh <shishir29121997@gmail.com>
2021-04-10 18:41:43 +0530

    initial setup

---
+++ packages/customer-service/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const port=process.env.PORT || 3003;
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("I am a customer API server");
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error: ${err.message}`);
+    }else{
+        console.log(`Listening on port ${port}`);
+    }
+})
---
+++ packages/payment-service/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios'); //for testing lerna out
+const express = require("express");
+const port=process.env.PORT || 3004;
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("I am a payment API server");
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(`Error: ${err.message}`);
+    }else{
+        console.log(`Listening on port ${port}`);
+    }
+})
